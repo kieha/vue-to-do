d2355b0e4b6075bd86e271b3620da3f8dbd94711
Njeri Kieha <sylvia.kieha@andela.com>
2018-11-15 18:08:34 +0300

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,10 @@
 import { CREATE_TODO, DELETE_TODO, EDIT_TODO } from './types';
 
 export const actions = {
-	createTodo: ({ commit }, todo) => commit(CREATE_TODO, { ...todo, done: false }),
-	deleteTodo: ({ commit }, todo) => commit(DELETE_TODO, todo),
-	editTodo: ({ commit }, todo) => commit(EDIT_TODO, { todo }),
-	toggleTodo: ({ commit }, todo) => commit(EDIT_TODO, { todo, done: !todo.done }),
+  createTodo: ({ commit }, todo) => commit(CREATE_TODO, { ...todo, done: false }),
+  deleteTodo: ({ commit }, todo) => commit(DELETE_TODO, todo),
+  editTodo: ({ commit }, todo) => commit(EDIT_TODO, { todo }),
+  toggleTodo: ({ commit }, todo) => commit(EDIT_TODO, { todo, done: !todo.done }),
 };
 
 export default actions;
